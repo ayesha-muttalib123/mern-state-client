4cfb852bf774541fd33bca1a73c76be297269220
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2024-08-19 18:03:23 +0500

    change vite.config for vercel backeend url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // your backend server URL
+        target: 'mern-state-backend.vercel.app', // your backend server URL
         changeOrigin: true,
         secure: false,
       },
